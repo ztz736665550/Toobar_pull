aaf49e0493b1b38452bacd2213d6d1fe4fcc46f5
ztz <736665550@qq.com>
2018-02-03 08:41:56 +0800

    下划渐变搜索框背景

--- app/src/main/java/com/example/ztz/toobar_pull/MainActivity.java
+++ app/src/main/java/com/example/ztz/toobar_pull/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     head_search_rr.setBackgroundColor(Color.argb((int) alpha, 255, 255, 255));
                 } else {
                     //将标题栏的颜色设置为完全不透明状态
+
                     head_search_rr.setBackgroundResource(R.color.white);
                 }
             }
